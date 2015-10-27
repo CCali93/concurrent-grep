302ae05001cb26e5473238c11070fdcf9653f3fc
Curtis Cali <botkilla2k12@gmail.com>
2015-10-27 18:07:21 -0400

    Added an executor.

--- src/CGrep.java
+++ src/CGrep.java
@@ -1,3 +1,6 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**n
  * Created by curtis on 10/27/15.
  */
@@ -10,6 +13,13 @@ public class CGrep {
             System.out.println("ERROR! Must provide a regex pattern");
         } else {
             String regex = args[0];
+            ExecutorService grepPool = Executors.newCachedThreadPool();
+
+            if (args.length >= 2) {
+                //Files are the input, loop through files
+            } else {
+                //Take input from stdin
+            }
         }
 
         System.out.println("Hello World");
--- src/TextExtractor.java
+++ src/TextExtractor.java
@@ -1,8 +1,15 @@
+import java.util.concurrent.Executor;
+
 /**
  * Created by curtis on 10/27/15.
  */
-public class TextExtractor {
+public class TextExtractor implements Executor {
     public TextExtractor() {
+
+    }
+
+    @Override
+    public void execute(Runnable command) {
 
     }
 }
